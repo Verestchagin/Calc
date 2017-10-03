ec9efcd3ade20c222e782ce20443e10d005b19c3
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2017-10-03 10:21:56 +0300

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -1,28 +1,89 @@
 #include <iostream>
-#include <stdio.h>
+#include <cmath>
+using namespace std;
 
-int main(int argc, char *argv[]){
-    std::cout << "Count = " << argc - 1 << std::endl;
-    for (int i = 1; i < argc; i++){
-        std::cout << "Argv[" << i << "] = " << argv[i] << std::endl;
-    }
 
-    int res = atoi(argv[1]);
-    for (int i = 2; i < argc; i = i + 2){
-        switch(argv[i][0]){
-            case '+':
-            res = res + atoi(argv[i + 1]);
-            break;
-            case '-':
-            res = res - atoi(argv[i + 1]);
-            break;
-            case '*':
-            res = res * atoi(argv[i + 1]);
-            break;
-            case '/':
-            res = res / atoi(argv[i + 1]);
-            break;
-        }
-    }
-    std::cout <<"Result: " << res << std::endl;
+int main()
+{
+	char op;
+	int a, b, res;
+	while (true) {
+		cout << "Viberete operaciyu [+, -, *, /, %, ^, !, &, |, <, >]" << endl;
+		cin >> op;
+		cout << "Vvedite pervoe chislo" << endl;
+		cin >> a;
+		if ((op != '!') && (op != '<') && (op != '>')) {
+			cout << "Vvedite vtoroe chislo" << endl;
+			cin >> b;	
+		}
+		switch (op) {
+			case '+':
+				res = a + b;
+				cout << "Result: " << res << endl;
+				break;
+			case '-':
+				res = a - b;
+				cout << "Result: " << res << endl;
+				break;
+			case '*':
+				res = a * b;
+				cout << "Result: " << res << endl;
+				break;
+			case '/':
+				if (b != 0) {
+					float a1 = a, b1 = b, res1;
+					res1 = a1 / b1;
+					cout << "Result: " << res1 << endl;
+					break;
+				}
+				else {
+					cout << "Nevozmozhnoe deistvie" << endl;
+					break;
+				}
+			case '%':
+				if (b != 0) {
+					res = a % b;
+					cout << "Result: " << res << endl;
+					break;
+				}
+				else {
+					cout << "Nevozmozhnoe deistvie" << endl;
+				}
+			case '^':
+				if (b == 0) {
+					res = 1;
+				}
+				else {
+					res = 1;
+					for (int i = 0; i < b; ++i) {
+						res = res * a;
+					}
+
+				}
+				cout << "Result: " << res << endl;
+				break;
+			case '!':
+				res = a * (-1);
+				cout << "Result: " << res << endl;
+				break;
+			case '&':
+				res = a & b; 
+				cout << "Result: " << res << endl;
+				break;
+			case '|':
+				res = a | b;
+				cout << "Result: " << res << endl;
+				break;
+			case '<':
+				res = a << 1;
+				cout << "Result: " << res << endl;
+				break;
+			case '>':
+				res = a >> 1;
+				cout << "Result: " << res << endl;
+				break;
+			default:
+				cout << "Neverniy znak" << endl;
+		}
+	}
 }
