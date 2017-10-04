84e1c22b0a1415b894206ba163da52b66d420ee8
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2017-10-04 23:51:04 +0300

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -1,19 +1,50 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+//#include <stdfax.h>
 #include <iostream>
 #include <cmath>
+#include <cctype>
+#include <cstdlib>
 using namespace std;
+
 
 int main()
 {
-	char op;
-	int a, b, res;
+	char op = '0';
+	int res,b,a;
+	char a1[] = "", b1[] = "";
+	bool f1 = false, f2 = false;
 	while (true) {
-		cout << "Viberete operaciyu [+, -, *, /, %, ^, !, &, |, <, >]" << endl;
-		cin >> op;
-		cout << "Vvedite pervoe chislo" << endl;
-		cin >> a;
+		while ((op != '+') && (op != '-') && (op != '*') && (op != '/') && (op != '%') && (op != '^') && (op != '!') && (op != '&') && (op != '|') && (op != '<') && (op != '>')) {
+			cout << "Viberete operaciyu [+, -, *, /, %, ^, !, &, |, <, >]" << endl;
+			cin >> op;
+		}
+		while (f1 == false) {
+			cout << "Vvedite pervoe chislo" << endl;
+			cin >> a1;
+			if (isdigit(a1[0])) {
+				a = atoi(a1);
+				f1 = true;
+				break;
+			}
+			else {
+				cout << "Neobhodimo vvesti chislo" << endl;
+			}
+		}
 		if ((op != '!') && (op != '<') && (op != '>')) {
-			cout << "Vvedite vtoroe chislo" << endl;
-			cin >> b;	
+			while (f2 == false) {
+				cout << "Vvedite vtoroe chislo" << endl;
+				cin >> b1;
+				if (isdigit(b1[0])) {
+					b = atoi(b1);
+					f2 = true;
+					break;
+				}
+				else {
+					cout << "Neobhodimo vvesti chislo" << endl;
+				}
+			}
 		}
 		switch (op) {
 			case '+':
@@ -81,8 +112,8 @@ int main()
 				res = a >> 1;
 				cout << "Result: " << res << endl;
 				break;
-			default:
-				cout << "Neverniy znak" << endl;
 		}
 	}
+    return 0;
 }
+
