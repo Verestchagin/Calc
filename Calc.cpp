c73e4b995ac25e7190c79731f3571d3a595ee36b
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2017-10-06 00:23:20 +0300

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -11,12 +11,12 @@ using namespace std;
 
 int main()
 {
-	char op = '0';
+	char op[] = "";
 	int res,b,a;
 	char a1[] = "", b1[] = "";
 	bool f1 = false, f2 = false;
 	while (true) {
-		while ((op != '+') && (op != '-') && (op != '*') && (op != '/') && (op != '%') && (op != '^') && (op != '!') && (op != '&') && (op != '|') && (op != '<') && (op != '>')) {
+		while ((op[0] != '+') && (op[0] != '-') && (op[0] != '*') && (op[0] != '/') && (op[0] != '%') && (op[0] != '^') && (op[0] != '!') && (op[0] != '&') && (op[0] != '|') && (op[0] != '<') && (op[0] != '>')) {
 			cout << "Viberete operaciyu [+, -, *, /, %, ^, !, &, |, <, >]" << endl;
 			cin >> op;
 		}
@@ -32,7 +32,7 @@ int main()
 				cout << "Neobhodimo vvesti chislo" << endl;
 			}
 		}
-		if ((op != '!') && (op != '<') && (op != '>')) {
+		if ((op[0] != '!') && (op[0] != '<') && (op[0] != '>')) {
 			while (f2 == false) {
 				cout << "Vvedite vtoroe chislo" << endl;
 				cin >> b1;
@@ -46,70 +46,109 @@ int main()
 				}
 			}
 		}
-		switch (op) {
+		switch (op[0]) {
 			case '+':
 				res = a + b;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '-':
 				res = a - b;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '*':
 				res = a * b;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '/':
 				if (b != 0) {
 					float a1 = a, b1 = b, res1;
 					res1 = a1 / b1;
+					f1 = false;
+					f2 = false;
+					op[0] = '0';
 					cout << "Result: " << res1 << endl;
 					break;
 				}
 				else {
 					cout << "Nevozmozhnoe deistvie" << endl;
+					f2 = false;
 					break;
 				}
 			case '%':
 				if (b != 0) {
 					res = a % b;
+					f1 = false;
+					f2 = false;
+					op[0] = '0';
 					cout << "Result: " << res << endl;
 					break;
 				}
 				else {
 					cout << "Nevozmozhnoe deistvie" << endl;
+					f2 = false;
+					break;
 				}
 			case '^':
 				if (b == 0) {
 					res = 1;
+					f1 = false;
+					f2 = false;
+					op[0] = '0';
+					break;
 				}
 				else {
 					res = 1;
 					for (int i = 0; i < b; ++i) {
 						res = res * a;
 					}
-
 				}
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '!':
 				res = a * (-1);
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '&':
 				res = a & b; 
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '|':
 				res = a | b;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '<':
 				res = a << 1;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 			case '>':
 				res = a >> 1;
+				f1 = false;
+				f2 = false;
+				op[0] = '0';
 				cout << "Result: " << res << endl;
 				break;
 		}
